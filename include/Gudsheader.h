adee5bb7c6baa9066d7644b2f274a1d4aca36dfc
TheCyberiousPizzerious <Trygve@harmaele.no>
2024-01-26 16:49:06 +0100

    gudsheader

--- include/Gudsheader.h
+++ include/Gudsheader.h
@@ -1,16 +1,11 @@
 #ifndef GUDSHEADER_H
 #define GUDSHEADER_H
 
-// #include <Hva som skal includes>
+class string_array
+{
+    public:
+        char gaming[4] = {'h', 'e', 'l', 'o'};
+};
+// #include <Hva som skal includes> 
 
 #endif
-
-
-
-
-
-15 VA
-
-88 muhammed Cade
-
-engebråten erik
