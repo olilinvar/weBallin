2131a566a9be888ffc49caaea9f75b2c4b729c15
TheCyberiousPizzerious <125442124+TheCyberiousPizzerious@users.noreply.github.com>
2024-01-31 21:21:18 +0100

    frontend

---
+++ website/frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ButtonMaxxing from './components/Button';
+
+function App() {
+  return (
+    <div>
+      <h1> Welcome to my app </h1>
+      <ButtonMaxxing title="I'm a working button" disabled={false} />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
