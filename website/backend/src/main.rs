1498443a38d95cb48e986752cf48ec56705a4149
TheCyberiousPizzerious <125442124+TheCyberiousPizzerious@users.noreply.github.com>
2024-01-31 21:15:17 +0100

    backend mappe laget og begynt

---
+++ website/backend/src/main.rs
@@ -0,0 +1,16 @@
+use actix_web::{get, App, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    "Hello, Actix web!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(hello)
+    })
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
