0d6329eb0f15cb416dd70bac9dfc1582283c5550
TheCyberiousPizzerious <Trygve@harmaele.no>
2024-02-28 16:36:11 +0100

    startet paa backend

--- website/backend/src/main.rs
+++ website/backend/src/main.rs
@@ -1,18 +1,18 @@
+mod api;
+mod models;
+mod repository;
+
 use actix_web::{get, App, HttpServer, Responder, HttpResponse};
 use actix_web::http::header;
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok()
-        .header(header::CONTENT_TYPE, "text/plain")
-        .body("Hello, Actix web!")
+    HttpResponse::Ok().json("Hello, you should KILL YOURSELF NOW!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().service(hello)
-    })
+    HttpServer::new(|| {App::new().service(hello)})
         .bind("0.0.0.0:8080")?
         .run()
         .await
